a8034abc5665b4dc5b97c364ef2685a281922b6b
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2020-07-27 17:59:36 +0900

    최댓값 구하기
    백준 문제 최댓값 구하기

---
+++ baekjoon Max.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N = 0;
+	int arr[9];
+	int max = 0;
+	int input;
+
+	for (int i = 0; i < 9; ++i)
+	{
+		
+		arr[i] = i;
+
+		cout << arr << endl;
+
+	}
+	
+	
+
+	return 0;
+}
